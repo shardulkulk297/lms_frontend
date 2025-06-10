9984ce63673553539affc03225fd272c84145b68
Shardul <shardulkulk297@gmail.com>
2025-06-10 17:53:28 +0530

    Did basics of react

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import Concepts from "./components/Concepts"
+import Tasks from "./components/Tasks"
+import Table from "./components/Table"
+import ApiDisplay from "./components/ApiDisplay"
+
+function App() {
+
+  
+  return (
+    <div>
+      
+      {/* <Concepts /> */}
+      {/* <Tasks /> */}
+      {/* <Table /> */}
+      <ApiDisplay />
+      
+    </div>
+  )
+}
+
+export default App
